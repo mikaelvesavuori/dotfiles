e705edf9ab2e8347738d9acaa0c8ccc855825478
Mikael Vesavuori <mikael.vesavuori@humblebee.se>
2020-01-05 10:42:42 +0100

    Update

--- setup-mac.sh
+++ setup-mac.sh
@@ -1,9 +1,12 @@
-echo "Setting up Mac with Xcode command-line utilities, Homebrew, Homebrew packages/casks, and dotfiles.";
+echo "Setting up Mac with Xcode command-line utilities, Homebrew, Homebrew packages/casks, Oh My Zsh, and dotfiles.";
 
+# Xcode Command-Line tools
 xcode-select --install
 
-# Check for Homebrew,
-# Install if we don't have it
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -14,6 +17,13 @@ cd brew
 brew bundle
 
 cd ..
+
+# Install Powerline fonts
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
 
 sh setup-dotfiles.sh
 
